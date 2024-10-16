a407c4514c59e74f90d3f487e9090310760edf83
Mael Duvey <mduvey@c1r2p7.42lehavre.fr>
2024-10-16 15:10:42 +0200

    fix:(all ft_is in header)

--- libft.h
+++ libft.h
@@ -6,18 +6,18 @@
 /*   By: mduvey <mduvey@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 15:51:22 by mduvey            #+#    #+#             */
-/*   Updated: 2024/10/15 16:30:37 by mduvey           ###   ########.fr       */
+/*   Updated: 2024/10/16 15:09:57 by mduvey           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <aio.h>
 
-int		ft_isalpha(char c);
-int		ft_isdigit(char c);
-int		ft_isalnum(char c);
-int		ft_isascii(char c);
-int		ft_isprint(char c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 int		ft_strlen(char *str);
 void	*ft_memset(void *s, int c, size_t n);
 void	bzero(void *s, size_t n);
